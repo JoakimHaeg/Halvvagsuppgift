248c1e0e8f67881c85bc0afca8e3de10a6b5f776
JoakimHaeg <Joakim.haegglund@icloud.com>
2023-10-19 20:48:18 +0200

    Commit dogshit

--- js/script.js
+++ js/script.js
@@ -8,13 +8,18 @@ let container = document.getElementById("container");
 function raceGen() {
     container.innerHTML = "";
 
-    let imgWidth = 100;
-    let imgHeight = 100;
+    let imgWidth = 50;
+    let imgHeight = 50;
+    let yOffset = 22.5;
+
     for (let i = 0; i < Math.floor(Math.random() * 10 + 2); i++) {
         let img = document.createElement("img");
         img.id = "car" + (i + 1);
         img.src = "/images/car.png";
         img.alt = "Car " + (i + 1);
+        img.style.position = "absolute";
+        img.style.left = "3.5%";
+        img.style.top = yOffset + i * 7.1 + "%";
 
         /* Använde mig av chatgpt då mitt förra försök av att modifiera färgen av car.png var misslyckande.
         Men förstår lite vad detta gör*/
@@ -27,7 +32,7 @@ function raceGen() {
             ctx.globalCompositeOperation = "source-in"; // har ingen aning vad detta är
             ctx.fillStyle = randomColor(); // har slumpar den färgen
             ctx.fillRect(0, 0, canvas.width, canvas.height); // har fyller den ritningen för att få en komplett bild
-            img.onload = null;
+            img.onload = null; // Så att den inte ändrar färg om och om igen
             img.src = canvas.toDataURL(); // har byter den från canvas till img
             container.appendChild(img);
         }
